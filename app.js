f504065bcbe5f36c983d3c59750de54a245c7057
Dan Stevens <1074238+DanStevens@users.noreply.github.com>
2021-06-15 22:34:34 +0100

    Using `os.hostname` didn't work as expected, so hardcode the domain name for the feed.

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 const express = require("express");
 const path = require("path");
-var os = require("os");
 const app = express();
 const PORT = process.env.PORT || 3000;
 const ALPHA_API_HOST = process.env.ALPHA_API_HOST || "api.alpha.org";
+const DOMAIN = "https://bioy-feed.herokuapp.com/";
 
 app.use(express.static(path.join(__dirname, "public")));
 
@@ -12,9 +12,9 @@ const alpha = require("./alpha-api");
 const client = new alpha.BioyClient(package.name, ALPHA_API_HOST);
 const feedifier = new  alpha.BioyEpisodeFeedifier();
 
-// Suffix feed image and icon with request host
-feedifier.feedOptions.imageUrl = "//" + os.hostname + feedifier.feedOptions.imageUrl;
-feedifier.feedOptions.favicon = "//" + os.hostname + feedifier.feedOptions.favicon;
+// Suffix feed image and icon with DOMAIN
+feedifier.feedOptions.imageUrl = DOMAIN + feedifier.feedOptions.imageUrl;
+feedifier.feedOptions.favicon = DOMAIN + feedifier.feedOptions.favicon;
 
 app.get("/", (req, res) => {
   res.redirect("/feed");
